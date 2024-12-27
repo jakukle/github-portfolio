f2278bb1ae1032ee8d54833b94e2d5f4738585d3
Jakub <jakub.klepacz@op.pl>
2024-12-27 12:26:06 +0100

    Learning C++ - University Studies
    It is assumed that the attendee of this lecture is familiar with the basics of structural programming and can program in C. However, no prior knowledge of C++, C#, or Java is assumed; elements of these languages will be introduced gradually from the basics in subsequent lectures.

---
+++ Object-oriented programming/Alokowanie tablic/Alokowanie tablic.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+
+double** allocMatrix2D(int, int);
+void deleteMatrix2D(double**&);
+
+int main(void)
+{
+    int i, j, nDim1 = 2, nDim2 = 3;
+    
+    //alokowanie
+    double** macierz2d = allocMatrix2D(nDim1, nDim2);
+
+    //wpisywanie wartości
+    for (i = 0; i < nDim1; i++)
+    {
+        for (j = 0; j < nDim2; j++)
+        {
+            macierz2d[i][j] = 3 * i + j + 1.5;
+        }
+    }
+    //drukowanie
+    for (i = 0; i < nDim1; i++)
+    {
+        for (j = 0; j < nDim2; j++)
+        {
+            cout << "Macierz2d[" << i << "][" << j << "] = " << macierz2d[i][j] << " ";
+        }
+        cout << endl;
+    }
+    //usuwanie
+    deleteMatrix2D(macierz2d);
+    return 0;
+}
+
+double** allocMatrix2D(int nDim1, int nDim2)
+{
+    double** macierz2d = new double* [nDim1];
+    double* pDumm = new double[nDim1 * nDim2];
+
+    for (int i = 0; i < nDim1; i++)
+    {
+        macierz2d[i] = pDumm + i * nDim2;
+    }
+    return macierz2d;
+}
+
+void deleteMatrix2D(double**& macierz2d)
+{
+    delete[] macierz2d[0];
+    delete[] macierz2d;
+    macierz2d = 0;
+}
---
+++ Object-oriented programming/Dziedziczenie Pies-Ssak/Dziedziczenie Pies-Ssak.cpp
@@ -0,0 +1,138 @@
+﻿#include <iostream>
+using namespace std;
+enum RASA { BULLTERIER, OWCZAREK, PITBULL, SHETLAND, DOBERMAN, LABRADOR };
+class Ssak
+{
+public:
+    //konstruktory
+    Ssak();
+    Ssak(int wiek);
+    ~Ssak();
+    //Funkcje dostepu
+    int PobierzWiek() const
+    {
+        return nJegoWiek;
+    }
+    void UstawWiek(int wiek)
+    {
+        nJegoWiek = wiek;
+    }
+    int PobierzWaga() const
+    {
+        return nJegoWaga;
+    }
+    void UstawWaga(int waga)
+    {
+        nJegoWaga = waga;
+    }
+    //Inne metody
+    void Mow() const
+    {
+        cout << "SSAK DAL GLOS\n";
+    }
+    void Spij() const
+    {
+        cout << "CICHO BO JA TERAZ SPIE\n";
+    }
+protected:
+    int nJegoWiek;
+    int nJegoWaga;
+};
+
+Ssak::Ssak() : nJegoWaga(5)
+{
+    nJegoWiek = 2;
+    cout << "KONSTRUKTOR SSAKA\n";
+}
+Ssak::Ssak(int wiek) : nJegoWiek(wiek), nJegoWaga(6)
+{
+    cout << "KONSTRUKTOR SSAKA (int)\n";
+}
+Ssak::~Ssak()
+{
+    cout << "DESTRUKTOR SSAKA\n";
+}
+
+class Pies : public Ssak
+{
+public:
+    //Konstruktory
+    Pies();
+    Pies(int wiek);
+    Pies(int wiek, int waga);
+    Pies(int wiek, RASA rasa);
+    Pies(int wiek, int waga, RASA rasa);
+    ~Pies();
+    //Funkcje dostepu
+    RASA PobierzRasa() const
+    {
+        return nJegoRasa;
+    }
+    void UstawRasa(RASA rasa)
+    {
+        nJegoRasa = rasa;
+    }
+    //Inne metody
+    void MachajOgonem()
+    {
+        cout << "Macham sobie ogonkiem\n";
+    }
+    void ProsOJedzenie()
+    {
+        cout << "Daj jedzonko\n";
+    }
+private:
+    RASA nJegoRasa;
+};
+
+
+Pies::Pies() : Ssak(), nJegoRasa(LABRADOR)
+{
+    cout << "KONSTRUKTOR PSA\n";
+}
+Pies::Pies(int wiek) : Ssak(wiek), nJegoRasa(LABRADOR)
+{
+    nJegoWiek = 1932;
+    cout << "KONSTRUKTOR PSA (int)\n";
+}
+Pies::Pies(int wiek, int waga) : Ssak(wiek), nJegoRasa(LABRADOR)
+{
+    nJegoWaga = waga;
+    cout << "KONSTRUKTOR PSA (int,int)\n";
+}
+Pies::Pies(int wiek, int waga, RASA rasa) : Ssak(wiek), nJegoRasa(rasa)
+{
+    nJegoWaga = waga;
+    cout << "KONSTRUKTOR PSA(int,int,RASA)\n";
+}
+Pies::Pies(int wiek, RASA rasa) : Ssak(wiek), nJegoRasa(rasa)
+{
+    cout << "KONSTRUKTOR PSA (int,RASA)\n";
+}
+Pies::~Pies()
+{
+    cout << "DESTRUKTOR PSA\n";
+}
+
+
+int main()
+{
+    Pies Marcin;
+    cout << "Marcin ma " << Marcin.PobierzWiek() << " lat\n";
+    cout << "Marcin ma " << Marcin.PobierzWaga() << " kilo\n";
+    Marcin.Mow();
+    Pies Filip(31);
+    cout << "Filip ma " << Filip.PobierzWiek() << " lat\n";
+    cout << "Filip ma " << Filip.PobierzWaga() << " kilo\n";
+    Filip.MachajOgonem();
+    Pies Dawid(32, 33);
+    cout << "Dawid ma " << Dawid.PobierzWiek() << " lat\n";
+    cout << "Dawid ma " << Dawid.PobierzWaga() << " kilo\n";
+    Pies Mateusz(34, BULLTERIER);
+    cout << "Mateusz ma " << Mateusz.PobierzWiek() << " lat\n";
+    cout << "Mateusz ma " << Mateusz.PobierzWaga() << " kilo\n";
+    Pies Sebastian(35, 36, DOBERMAN);
+    cout << "Sebastian ma " << Sebastian.PobierzWiek() << " lat\n";
+    cout << "Sebastian ma " << Sebastian.PobierzWaga() << " kilo\n";
+    return 0;
+}
